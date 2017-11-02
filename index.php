cecabfae3f00962d14c8af803d51c6f4a3835a42
SanSany4 <tselikov.alexander@gmail.com>
2017-11-02 04:18:59 +0400

    It's working

---
+++ index.php
@@ -0,0 +1,6 @@
+<form action="lucky.php" method="post">
+    <p>distribution sample size: <input type="text" name="sample" /></p>
+    <p>bar height: <input type="text" name="height" /></p>
+    <p>bar width: <input type="text" name="width" /></p>
+    <p><input type="submit" /></p>
+</form>
---
+++ lucky.php
@@ -0,0 +1,66 @@
+<?php
+
+function add_zeroes($str) {   //additional zeroes in the begin
+    $s = "$str";
+    while (strlen($s) < 6) {
+        $s = "0" . $s;
+    }
+    return $s;
+}
+
+function is_lucky($ticket) {                         //check if ticket is lucky
+    $t = add_zeroes($ticket);
+    $first = (int)$t[0] + (int)$t[1] + (int)$t[2];   //first and...
+    $second = (int)$t[3] + (int)$t[4] + (int)$t[5];  //last three digits..
+    return $first == $second;                        //compared
+}
+
+$sample = (int)$_POST['sample'];                     //size of distribution sample
+$height = (int)$_POST['height'];                     //height of graphic / maximum height of bar
+$width = (int)$_POST['width'];                       //width of single bar
+$result = array();
+$max = 0;                                            //maximum of lucky tickets in single sample between all samples
+$count = 0;                                          //lucky tickets in current sample
+$left = 0;                                           //left border of current sample
+$right = $sample - 1;                                //right border of current sample
+for($i = 0; $i < 1000000; ++$i) {
+    if (is_lucky($i)) {
+        ++$count;
+    }
+    if ($i == $right) {                              //if current sample is ended
+        if ($count > $max) {                         //updating maximum
+            $max = $count;
+        }
+        $result[] = array('left' => $left, 'right' => $right, 'count' => $count);//saving it
+        $count = 0;                                  //resetting counter
+        $left = $right + 1;                          //new borders
+        $right = $left + $sample - 1;
+    }
+}
+$result[] = array('left' => $left, 'right' => 999999, 'count' => $count);  //adding the last tickets out of sample
+
+foreach ($result as $value) {
+    $percent = 100 * $value['count'] / $max;
+    $l = add_zeroes($value['left']);
+    $r = add_zeroes($value['right']);
+    $p = number_format($percent, 2);
+    echo "$l .. $r: {$value['count']}  $p% <br>";    //outputting info about each sample
+}
+?>
+<br>
+<embed type="image/svg+xml">
+    <?php echo '<?xml version="1.0 encoding="UTF-8"?>'; ?>
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="<?=($width * count($result) + 20)?>" height="<?=$height?>">
+        <?php
+        //generating SVG
+        $x = 10;    //offset
+        foreach ($result as $value) {
+            $h = (int)($height * $value['count'] / $max);  //relative height of column
+            $y = $height - $h;
+            echo "<rect x=\"$x\" y=\"$y\" width=\"$width\" height=\"$h\" fill=\"teal\"/>"; //drawing column
+            $x += $width; //moving to next column
+        }
+
+        ?>
+    </svg>
+</embed>
